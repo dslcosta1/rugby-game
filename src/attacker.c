d5ec9bfd4ef74fcc32deff018031925c04529cdc
Daniel Silva Lopes da Costa <dslcosta2016@usp.br>
2022-03-26 15:19:55 -0300

    Add Defender and Atacker Strategy
    - Both use SPY and based on the information they change the behaviour.
    - There is a random strategy for when the player is stucked.
    
    Signed-off-by: Daniel Silva Lopes da Costa <dslcosta2016@usp.br>

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 // Internal headers
 #include "direction.h"
@@ -12,18 +14,81 @@
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
+// Global Variables
+int TURN_ATTACKER = 0;
+int LAST_DIRECTION_MOVED_ATTACKER = 0;
+int COIN = -1;
+position_t LAST_POSITION_ATTACKER = {-1, -1};
+position_t INICIAL_POSITION_ATTACKER = {-1, -1};
+position_t POSITION_DEFENDER_SPY = {-1, -1};
+
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_attacker_strategy(
-    position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
+    position_t attacker_position, Spy defender_spy)
+{
+  direction_t direction_list[10] = {DIR_UP, DIR_DOWN, DIR_LEFT, DIR_RIGHT, DIR_DOWN_LEFT, DIR_DOWN_RIGHT, DIR_UP_LEFT, DIR_UP_RIGHT, DIR_DOWN_RIGHT, DIR_UP_RIGHT};
+  int mov;
+  TURN_ATTACKER++;
+  srand(time(0));
 
-  // TODO: Implement Attacker logic here
-  return DIR_RIGHT;
+  //Save the initial position
+  if ((int)INICIAL_POSITION_ATTACKER.i == -1)
+  {
+    INICIAL_POSITION_ATTACKER.i = attacker_position.i;
+    INICIAL_POSITION_ATTACKER.j = attacker_position.j;
+    COIN = rand() % 2;
+  }
+
+  // Hit a barrier - need to move to another direction
+  if (equal_positions(LAST_POSITION_ATTACKER, attacker_position))
+  {
+    mov = rand() % 10;
+    while (mov == LAST_DIRECTION_MOVED_ATTACKER)
+    {
+      mov = rand() % 10;
+    }
+    LAST_DIRECTION_MOVED_ATTACKER = mov;
+    return direction_list[mov];
+  }
+
+  LAST_POSITION_ATTACKER = attacker_position;
+
+  if (TURN_ATTACKER < (int)INICIAL_POSITION_ATTACKER.i + 2)
+  {
+    // Main strategy before the SPY
+    mov = 8 + COIN;
+    LAST_DIRECTION_MOVED_ATTACKER = mov;
+    return direction_list[mov];
+  }
+  else
+  {
+    //Use SPY
+    if ((int)POSITION_DEFENDER_SPY.i == -1)
+    {
+      POSITION_DEFENDER_SPY = get_spy_position(defender_spy);
+      COIN = (COIN + 1) % 2;
+    }
+
+    if ((int)POSITION_DEFENDER_SPY.j <= (int)attacker_position.j)
+    {
+      mov = 3;
+    }
+    else
+    {
+      mov = 8 + COIN;
+    }
+
+    mov = rand() % 4 == 0;
+    LAST_DIRECTION_MOVED_ATTACKER = mov;
+    return direction_list[mov];
+  }
+
+  mov = 3;
+  LAST_DIRECTION_MOVED_ATTACKER = mov;
+  return direction_list[mov];
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
@@ -12,18 +14,91 @@
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
+// Global Variables
+int TURN_DEFENDER = 0;
+int LAST_DIRECTION_MOVED_DEFENDER = -1;
+position_t LAST_POSITION_DEFENDER = {-1, -1};
+position_t INICIAL_POSITION_DEFENDER = {-1, -1};
+position_t POSITION_ATTACKER_SPY = {-1, -1};
+
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_defender_strategy(
-    position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
+    position_t defender_position, Spy attacker_spy)
+{
+  direction_t direction_list[10] = {DIR_UP, DIR_DOWN, DIR_UP, DIR_DOWN, DIR_LEFT, DIR_RIGHT, DIR_DOWN_LEFT, DIR_DOWN_RIGHT, DIR_UP_LEFT, DIR_UP_RIGHT};
+  int mov;
+  TURN_DEFENDER++;
+  srand(time(0));
 
-  // TODO: Implement Defender logic here
-  return DIR_LEFT;
+  //Save the initial position
+  if ((int)INICIAL_POSITION_DEFENDER.i == -1)
+  {
+    INICIAL_POSITION_DEFENDER.i = defender_position.i;
+    INICIAL_POSITION_DEFENDER.j = defender_position.j;
+  }
+
+  // Hit a barrier - need to move to another direction
+  if (equal_positions(LAST_POSITION_DEFENDER, defender_position))
+  {
+    mov = rand() % 10;
+    while (mov == LAST_DIRECTION_MOVED_DEFENDER)
+    {
+      mov = rand() % 10;
+    }
+    LAST_DIRECTION_MOVED_DEFENDER = mov;
+    return direction_list[mov];
+  }
+
+  LAST_POSITION_DEFENDER = defender_position;
+
+  if (TURN_DEFENDER < ((int)INICIAL_POSITION_DEFENDER.j) / 2 + 1)
+  {
+    // Main strategy before the SPY
+
+    if (abs((int)defender_position.j - (int)INICIAL_POSITION_DEFENDER.j) < 2)
+    {
+      mov = 4;
+      LAST_DIRECTION_MOVED_DEFENDER = mov;
+      return direction_list[mov];
+    }
+    else
+    {
+      mov = rand() % 4 == 0;
+      LAST_DIRECTION_MOVED_DEFENDER = mov;
+      return direction_list[mov];
+    }
+  }
+  else
+  {
+    //Use SPY
+    if ((int)POSITION_ATTACKER_SPY.i == -1)
+    {
+      POSITION_ATTACKER_SPY = get_spy_position(attacker_spy);
+    }
+
+    direction_t direction_list_spy[4] = {DIR_UP, DIR_UP, DIR_DOWN, DIR_DOWN};
+
+    if ((int)POSITION_ATTACKER_SPY.i > (int)defender_position.i)
+    {
+      direction_list_spy[2] = DIR_UP;
+    }
+
+    if ((int)POSITION_ATTACKER_SPY.i < (int)defender_position.i)
+    {
+      direction_list_spy[1] = DIR_DOWN;
+    }
+
+    mov = rand() % 4 == 0;
+    LAST_DIRECTION_MOVED_DEFENDER = mov;
+    return direction_list_spy[mov];
+  }
+
+  mov = 4;
+  LAST_DIRECTION_MOVED_DEFENDER = mov;
+  return direction_list[mov];
 }
 
 /*----------------------------------------------------------------------------*/
